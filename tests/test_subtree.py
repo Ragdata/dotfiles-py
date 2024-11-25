7b86e1d7d596553884a498f0d481db6dbe5bf843
Ragdata <ragdata@users.noreply.github.com>
2024-11-25 13:11:26 +1000

    test: add subtree tests

--- tests/test_subtree.py
+++ tests/test_subtree.py
@@ -16,6 +16,9 @@ copyright:      Copyright © 2024 Redeyed Technologies
 import os
 
 from pathlib import Path
+from typer.testing import CliRunner
+from dotfiles_py.subtree.cli import app
+from dotfiles_py.subtree.models import Subtree
 ####################################################################
 # ATTRIBUTES
 ####################################################################
@@ -23,6 +26,34 @@ from pathlib import Path
 ####################################################################
 # MODULES
 ####################################################################
-def test_subtree(settings):
-    """`settings` is a fixture defined in conftest.py"""
+runner = CliRunner()
 
+def test_callback():
+    result = runner.invoke(app)
+    assert result.exit_code == 0
+
+def test_version():
+    result = runner.invoke(app, ["version"])
+    assert result.exit_code == 0
+
+def test_add(settings):
+
+    treefile = Path(settings.get('dir.repo'))
+    treefile = treefile.joinpath('tests/data/.subtrees.yml')
+
+    subtree = Subtree(treefile)
+
+    assert treefile.exists() == True
+
+
+def test_list():
+    pass
+
+def test_fetch():
+    pass
+
+def test_pull():
+    pass
+
+def test_remove():
+    pass
