ba9fffd896277e3d8528e8adf8346bc4a68dcf91
Ragdata <ragdata@users.noreply.github.com>
2024-11-30 00:12:00 +1000

    refactor: update return codes

--- dotfiles_py/subtree/cli.py
+++ dotfiles_py/subtree/cli.py
@@ -30,6 +30,7 @@ from dotfiles_py.subtree.models import Subtree
 # MODULES
 ####################################################################
 app = typer.Typer(rich_markup_mode="rich", context_settings={'help_option_names': ['-h', '--help']}, no_args_is_help=True)
+subtree = Subtree()
 
 @app.callback(invoke_without_command=True)
 def callback(version: bool = typer.Option(False, '-V', '--version', help="Display the submodule version and exit", is_eager=True)) -> int:
@@ -40,39 +41,39 @@ def callback(version: bool = typer.Option(False, '-V', '--version', help="Displa
         print(f"{__app_name__.capitalize()} [yellow]v{__app_version__}[/yellow]")
         raise typer.Exit(1)
 
-    return sys.exit(0)
+    return 0
 
-@app.command()
+@app.command('add')
 def add(label: str, path: str, url: str,
     branch: Annotated[Optional[str], typer.Option()] = "master",
     squash: Annotated[Optional[bool], typer.Option()] = True,
     message: Annotated[Optional[str], typer.Option()] = None
 ) -> int:
     """Add a subtree to the current repository"""
-    Subtree().add(label, path, url, branch, squash, message)
-    return sys.exit(0)
+    subtree.add(label, path, url, branch, squash, message)
+    return 0
 
 @app.command('fetch')
 def fetch(label: str) -> int:
     """Perform a `git fetch` for the named subtree"""
-    return sys.exit(0)
+    return 0
 
 @app.command('list')
 def show() -> int:
     """List all currently installed subtrees"""
-    Subtree().show()
-    return sys.exit(0)
+    subtree.show()
+    return 0
 
 @app.command('pull')
 def pull(label: str) -> int:
     """Perform a `git pull` for the named subtree"""
-    return sys.exit(0)
+    return 0
 
 @app.command('remove')
 def remove(label: str) -> int:
     """Remove the named subtree from the current repository"""
-    Subtree().remove(label)
-    return sys.exit(0)
+    subtree.remove(label)
+    return 0
 
 @app.command('version')
 def module_version() -> int:
